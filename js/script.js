f3e5d5d6e4e85203f859c6cb38d154692a0f7ad9
Keakone <keanucarnevale@gmail.com>
2016-03-02 10:14:54 +1300

    finalised product

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 var result = ""; //must be a string so it concatenates rather than adds
+var nonNum = false; //prevents user from pressing non numbers multiple times in a row
 
 function calc(digit){
 
@@ -6,15 +7,45 @@ function calc(digit){
         var prevResult = result;
         result = prevResult;
         $("#resultBox").append("Ans");
+        nonNum = true;
     }
     else if (digit == "sum"){
         $("#resultBox").val(eval(result));
+        nonNum = true;
     }
     else if (digit == "clear"){
         location.reload();
     }
-    else{
+    else if (isNaN(digit) && nonNum == true){
+        result += digit;
+        nonNum = false; 
+    }
+    else if (!isNaN(digit)){
       result += digit; 
-      $("#resultBox").val(result);  
+      $("#resultBox").val(result);
+      nonNum = true;  
     }
 }
+
+// var result = ""; //must be a string so it concatenates rather than adds
+
+// function calc(digit){
+
+//     if (digit == "ans"){
+//         var prevResult = result;
+//         result = prevResult;
+//         $("#resultBox").append("Ans");
+//     }
+//     else if (digit == "sum"){
+//         $("#resultBox").val(eval(result));
+//     }
+//     else if (digit == "clear"){
+//         location.reload();
+//     }
+//     else{
+//       result += digit; 
+//       $("#resultBox").val(result);  
+//     }
+// }
+
+
